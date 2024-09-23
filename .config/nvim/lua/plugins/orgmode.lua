0a1a995bb2c24c3dff49a4ea408a303d31d49ad1
diego <diegoreste04@gmail.com>
2024-09-23 22:44:12 +0200

    changes to orgmode config and i3blocks

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -5,12 +5,18 @@ return {
   config = function()
     -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = {'~/diego/orgmode/**/*', '~/diego/uni/uni.org'},
+      org_agenda_files = {'~/diego/uni/y2s1/**/*', '~/diego/uni/y2s2/**/*'},
       org_default_notes_file = '~/diego/orgmode/notes/refile.org',
       org_todo_keywords = {'LECTURE(l)', 'TODO(t)', 'COULDO(c)', 'WORKINGON(w)', 'NOTDONE(n)', 'DONE(d)'},
       org_todo_repeat_to_state = "LECTURE",
-      win_split_mode = "vertical",
-      win_border = "single",
+      win_split_mode = "auto",
+      org_startup_folded = "showeverything",
+      org_hide_leading_stars = true,
+      org_agenda_skip_scheduled_if_done = true,
+      org_capture_templates = {
+        t = {description = 'task', template = '* TODO %?\n SCHEDULED: %T\n** notes \n\n %u'},
+        n = {description = 'note', template = ''},
+      },
     })
 
   end,
