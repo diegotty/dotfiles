41d8c8f398642c4a7eba00e35a3d696898836cb0
diego <diegoreste04@gmail.com>
2025-08-18 07:56:01 +0200

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,47 +17,29 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
-  -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " " 
 vim.g.maplocalleader ="\\"
+
+-- tab preferences
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
 -- relative numbers
 vim.opt.number = true
 vim.wo.relativenumber = true
 vim.splitright = true
 vim.splitbelow = true
+
+-- highlighting on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
   callback = function() vim.highlight.on_yank({timeout=450, higroup='Visual'}) end 
 })
-vim.g.have_nerd_font = true
-require("vim-options")
--- Setup lazy.nvim
-require("lazy").setup("plugins")
-vim.keymap.set('', '<up>', function () print('you can do it'); end)
-vim.keymap.set('', '<down>', function () print('still in hard mode'); end)
-vim.keymap.set('', '<left>', function () print('i believe in you'); end)
-vim.keymap.set('', '<right>', function () print('you\'re doing great'); end)
-vim.keymap.set('n', 'n', 'nzz')
-vim.keymap.set('n', 'N', 'Nzz')
-vim.keymap.set('', '<C-u>', '<C-u>zz')
-vim.keymap.set('', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<leader>c', ':noh<CR>')
 
--- vim.api.nvim_create_user_command("Run", function()
---   local path = vim.api.nvim_buf_getname(0);
---   print(path);
--- end, {})
---
-vim.keymap.set("n", "<leader>st", function()
-  -- vim.cmd("cd %:p:h")
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0,10)
-  vim.fn.feedkeys("a")
-end)
+-- nerd font
+vim.g.have_nerd_font = true
+require("keymaps")
+
+require("lazy").setup("plugins")
