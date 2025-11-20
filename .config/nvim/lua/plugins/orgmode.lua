21e1698b2e8bf4caa3deab93737f21d061645aed
diegotty <diegoreste04@gmail.com>
2025-11-20 19:05:45 +0100

    fixed orgmode bug for macbook

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -5,7 +5,8 @@ return {
   config = function()
     -- Setup orgmode
     require("orgmode").setup({
-      org_agenda_files = { "~/diego/orgmode/**/*", "~/diego/uni/y2s1/**/*", "~/diego/uni/y2s2/**/*" , "~/diego/uni/y3s1/**/*"},
+      org_agenda_use_time_grid = false,
+      org_agenda_files = { "~/diego/orgmode/**/*", "~/diego/uni/y2s1/**/*", "~/diego/uni/y2s2/**/*", "~/diego/uni/y3s1/**/*"},
       org_default_notes_file = "~/diego/orgmode/refile.org",
       org_todo_keywords = { "LECTURE(l)", "TODO(t)", "COULDO(c)", "WORKINGON(w)", "EXAM", "|", "NOTDONE(n)", "DONE(d)" },
       org_todo_keyword_faces = {
